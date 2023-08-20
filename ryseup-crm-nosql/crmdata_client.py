ab168f8dd83f8e15b6d791f957dd10c547857ba5
etaif725 <142711245+etaif725@users.noreply.github.com>
2023-08-20 22:02:03 +0000

    first commit of basic API

---
+++ ryseup-crm-nosql/crmdata_client.py
@@ -0,0 +1,15 @@
+import os
+
+import click
+import requests
+
+@click.command()
+@click.option("--userdata", default="Friend")
+def get_user_data(firstname):
+    headers = {"X-Github-Token": os.eviron.get("CRMDATA_GITHUB_TOKEN")}
+    url = f"https://redesigned-funicular-v6vrxqq4qxq4hw9q4-7071.app.github.dev/api/crmdata?name=[firstname]"
+    data = requests.get(url).json()
+    print(data[firstname])
+
+if __name__ == "__main__":
+    get_user_data()
